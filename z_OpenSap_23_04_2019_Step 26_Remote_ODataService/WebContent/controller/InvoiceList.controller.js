2b258965be044d2bad583e88871f60fdda3099c4
Zayidu A <33104525+zayidu@users.noreply.github.com>
2019-04-25 22:07:04 +0530

    Add files via upload

---
+++ z_OpenSap_23_04_2019_Step 26_Remote_ODataService/WebContent/controller/InvoiceList.controller.js
@@ -0,0 +1,57 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+	"use strict";
+
+	return Controller.extend("sap.ui.opensap.controller.InvoiceList", {
+		
+		formatter: formatter,
+		onInit : function () {
+			var oViewModel = new JSONModel({
+				currency: "INR"
+			});
+			this.getView().setModel(oViewModel, "view");
+			
+			
+			this.onDataLoad();
+		},
+		
+		onDataLoad : function(){
+			debugger;
+			var oModel;
+			oModel = this.getOwnerComponent().getModel("invoice");
+			
+			oModel.read("/Et_DataSet",{
+				  "success":function(odata,oResponse){
+				   
+				  },
+				  "error":function(oResponse){
+				   sap.m.MessageToast.show("fail");
+				  }
+				  
+				 });
+
+
+		},
+		
+		onFilterInvoices : function (oEvent) {
+			debugger;
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.byId("invoiceList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		}
+
+	});
+});
